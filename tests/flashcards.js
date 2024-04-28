efaefc517cad564f9480390bc4e482f6ca1ab6e4
Dillon Jeffers <dillon_jeffers@fastmail.com>
2024-04-27 23:54:14 -0400

    fix unit tests by resolving the expect function

--- tests/flashcards.js
+++ tests/flashcards.js
@@ -4,21 +4,21 @@ import flashcards from '../flashcards.js';
 test('getLanguageIndex()', () => {
     const randomIndex = flashcards.getLanguageIndex();
 
-    expect(Number.isInteger(randomIndex));
+    expect(Number.isInteger(randomIndex)).toBe(true);
 });
 
 test('getTargetLanguageIndex() with initial index of: 0', () => {
     const initialIndex = 0,
         randomIndex = flashcards.getTargetLanguageIndex(initialIndex);
 
-    expect(Number.isInteger(randomIndex));
-    expect(randomIndex !== initialIndex);
+    expect(Number.isInteger(randomIndex)).toBe(true);
+    expect(randomIndex !== initialIndex).toBe(true);
 });
 
 test('getTargetLanguageIndex() with initial index of: 3', () => {
     const initialIndex = 3,
         randomIndex = flashcards.getTargetLanguageIndex(initialIndex);
 
-    expect(Number.isInteger(randomIndex));
-    expect(randomIndex !== initialIndex);
+    expect(Number.isInteger(randomIndex)).toBe(true);
+    expect(randomIndex !== initialIndex).toBe(true);
 });
